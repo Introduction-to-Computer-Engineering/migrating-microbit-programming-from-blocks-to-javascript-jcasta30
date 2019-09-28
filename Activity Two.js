4ff89537b542e54a995f514491982c665c8e6552
jcasta30 <54787646+jcasta30@users.noreply.github.com>
2019-09-28 15:32:46 -0600

    Create Activity Two.js

---
+++ Activity Two.js
@@ -0,0 +1,23 @@
+// Activity Two: Happy Face, Sad Face
+
+// Display a happy face when button A is pressed.
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+// Display a sad face when button B is pressed.
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.clearScreen()
